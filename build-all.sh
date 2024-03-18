a98f3111c51ace88f57312636d8c5c9e5ad3d410
Miguel <1220772@isep.ipp.pt>
2024-03-18 10:52:35 +0000

    Fixing build scripts. #4

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ECHO OFF
-ECHO make sure JAVA_HOME is set to JDK folder
-ECHO make sure maven is on the system PATH
+ECHO Make sure JAVA_HOME is set to JDK folder
+ECHO Make sure maven is on the system PATH
 mvn $1 dependency:copy-dependencies package

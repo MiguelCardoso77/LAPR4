f7c6a58599fb073d8e41db4d5ff647f67349f22c
Miguel <1220772@isep.ipp.pt>
2024-04-17 14:37:59 +0100

    BackOffice Refactor #7

--- jobs4u.backoffice.app/src/main/java/Jobs4uBackOffice.java
+++ jobs4u.backoffice.app/src/main/java/Jobs4uBackOffice.java
@@ -1,18 +1,23 @@
-import app.Jobs4uBaseApplication;
-import authentication.AuthenticationCredentialHandler;
-import authentication.Jobs4uPasswordPolicy;
-import authentication.LoginUI;
+import presentation.MainMenu;
+import console.BaseApplication;
+import console.presentation.authz.LoginUI;
+import eapli.clientusermanagement.application.eventhandlers.NewUserRegisteredFromSignupWatchDog;
+import eapli.clientusermanagement.domain.events.NewUserRegisteredFromSignupEvent;
+import eapli.clientusermanagement.domain.events.SignupAcceptedEvent;
+import eapli.infrastructure.authz.AuthenticationCredentialHandler;
+import eapli.persistence.PersistenceContext;
+import eapli.usermanagement.application.eventhandlers.SignupAcceptedWatchDog;
+import eapli.usermanagement.domain.BasePasswordPolicy;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
 import eapli.framework.infrastructure.authz.domain.model.PlainTextEncoder;
 import eapli.framework.infrastructure.pubsub.EventDispatcher;
-import events.NewUserRegisteredFromSignupEvent;
-import events.NewUserRegisteredFromSignupWatchDog;
-import events.SignupAcceptedEvent;
-import events.SignupAcceptedWatchDog;
-import menus.MainMenu;
-import persistence.PersistenceContext;
 
-public class Jobs4uBackOffice extends Jobs4uBaseApplication {
+/**
+ *
+ * @author Paulo Gandra Sousa
+ */
+@SuppressWarnings("squid:S106")
+public final class Jobs4uBackOffice extends BaseApplication {
 
     /**
      * avoid instantiation of this class.
@@ -25,40 +30,35 @@ public class Jobs4uBackOffice extends Jobs4uBaseApplication {
      *            the command line arguments
      */
     public static void main(final String[] args) {
+        AuthzRegistry.configure(PersistenceContext.repositories().users(), new BasePasswordPolicy(), new PlainTextEncoder());
         new Jobs4uBackOffice().run(args);
     }
 
-    /**
-     * Method to run the application if the user is authenticated
-     */
     @Override
     protected void doMain(final String[] args) {
-        final boolean authenticated = new LoginUI(new AuthenticationCredentialHandler()).show();
-        if (authenticated) {
-            final var menu = new MainMenu();
+        // login and go to main menu
+        if (new LoginUI(new AuthenticationCredentialHandler()).show()) {
+            // go to main menu
+            final MainMenu menu = new MainMenu();
             menu.mainLoop();
         }
     }
 
     @Override
     protected String appTitle() {
-        return "Jobs4U Back Office";
+        return "Base Back Office";
     }
 
     @Override
     protected String appGoodbye() {
-        return "Jobs4U Back Office";
-    }
-
-    @Override
-    protected void configureAuthz() {
-        AuthzRegistry.configure(PersistenceContext.repositories().users(), new Jobs4uPasswordPolicy(), new PlainTextEncoder());
+        return "Base Back Office";
     }
 
     @SuppressWarnings("unchecked")
     @Override
     protected void doSetupEventHandlers(final EventDispatcher dispatcher) {
-        dispatcher.subscribe(new NewUserRegisteredFromSignupWatchDog(), NewUserRegisteredFromSignupEvent.class);
+        dispatcher.subscribe(new NewUserRegisteredFromSignupWatchDog(),
+                NewUserRegisteredFromSignupEvent.class);
         dispatcher.subscribe(new SignupAcceptedWatchDog(), SignupAcceptedEvent.class);
     }
 }
--- jobs4u.backoffice.app/src/main/java/hello3/Greeter.java
+++
@@ -1,7 +0,0 @@
-package hello3;
-
-public class Greeter {
-	public String sayHello() {
-		return "Hello world!";
-	}
-}

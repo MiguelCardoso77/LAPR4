b8f533ea7bfe3e68177ea93c99bc516bb41ee5b5
Miguel <1220772@isep.ipp.pt>
2024-04-17 12:33:09 +0100

    Fixing modules

--- jobs4u.backoffice.app/src/main/java/authentication/CredentialHandler.java
+++
@@ -1,7 +0,0 @@
-package authentication;
-
-import eapli.framework.infrastructure.authz.domain.model.Role;
-
-public interface CredentialHandler {
-    boolean authenticated(String username, String password, Role onlyWithThis);
-}

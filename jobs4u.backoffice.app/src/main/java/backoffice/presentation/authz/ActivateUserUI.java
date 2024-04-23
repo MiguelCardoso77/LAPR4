97d744ef9c5e5f54f0ec03d4942e836346392452
Diana Neves <1221194@isep.ipp.pt>
2024-04-23 17:34:21 +0100

    ActivateUserUI implementation #8

---
+++ jobs4u.backoffice.app/src/main/java/backoffice/presentation/authz/ActivateUserUI.java
@@ -0,0 +1,58 @@
+package backoffice.presentation.authz;
+
+import core.application.controllers.ActivateUserController;
+import core.application.controllers.DeactivateUserController;
+import eapli.framework.domain.repositories.ConcurrencyException;
+import eapli.framework.domain.repositories.IntegrityViolationException;
+import eapli.framework.infrastructure.authz.domain.model.SystemUser;
+import eapli.framework.io.util.Console;
+import eapli.framework.presentation.console.AbstractUI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivateUserUI extends AbstractUI {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ActivateUserUI.class);
+
+    private final ActivateUserController theController = new ActivateUserController();
+
+    @Override
+    protected boolean doShow() {
+        final List<SystemUser> list = new ArrayList<>();
+        final Iterable<SystemUser> iterable = this.theController.deactivatedUsers();
+        if (!iterable.iterator().hasNext()) {
+            System.out.println("There is no registered User");
+        } else {
+            int cont = 1;
+            System.out.println("SELECT User to activate\n");
+            // FIXME use select widget, see, ChangeDishTypeUI
+            System.out.printf("%-6s%-10s%-30s%-30s%n", "Nº:", "Username", "Firstname", "Lastname");
+            for (final SystemUser user : iterable) {
+                list.add(user);
+                System.out.printf("%-6d%-10s%-30s%-30s%n", cont, user.username(), user.name().firstName(),
+                        user.name().lastName());
+                cont++;
+            }
+            final int option = Console.readInteger("Enter user nº to activate or 0 to finish ");
+            if (option == 0) {
+                System.out.println("No user selected");
+            } else {
+                try {
+                    this.theController.activateUser(list.get(option - 1));
+                } catch (IntegrityViolationException | ConcurrencyException ex) {
+                    LOGGER.error("Error performing the operation", ex);
+                    System.out.println(
+                            "Unfortunately there was an unexpected error in the application. Please try again and if the problem persists, contact your system administrator.");
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String headline() {
+        return "Activate User";
+    }
+}

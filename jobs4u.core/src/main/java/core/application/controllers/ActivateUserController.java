2e9ac14b029cbbd963782126fe4885c268af143e
Diana Neves <1221194@isep.ipp.pt>
2024-04-23 17:34:55 +0100

    ActiveUserController implementation #8

---
+++ jobs4u.core/src/main/java/core/application/controllers/ActivateUserController.java
@@ -0,0 +1,25 @@
+package core.application.controllers;
+
+import core.domain.user.Jobs4URoles;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.infrastructure.authz.application.UserManagementService;
+import eapli.framework.infrastructure.authz.domain.model.SystemUser;
+
+public class ActivateUserController {
+
+    private final AuthorizationService authz = AuthzRegistry.authorizationService();
+    private final UserManagementService userSvc = AuthzRegistry.userService();
+
+    public Iterable<SystemUser> deactivatedUsers() {
+        authz.ensureAuthenticatedUserHasAnyOf(Jobs4URoles.POWER_USER, Jobs4URoles.ADMIN);
+
+        return userSvc.deactivatedUsers();
+    }
+
+    public SystemUser activateUser(final SystemUser user) {
+        authz.ensureAuthenticatedUserHasAnyOf(Jobs4URoles.POWER_USER, Jobs4URoles.ADMIN);
+
+        return userSvc.activateUser(user);
+    }
+}

5522e47880dada8127d27b145242e098c81f913a
Diana Neves <1221194@isep.ipp.pt>
2024-04-24 10:09:34 +0100

    Application Bootstrapper implementation

---
+++ jobs4u.bootstrappers/src/main/java/bootstrappers/bootstraping/ApplicationsBootstrapper.java
@@ -0,0 +1,9 @@
+package bootstrappers.bootstraping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ApplicationsBootstrapper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JobsBootstrapper.class);
+
+}

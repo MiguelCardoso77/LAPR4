bf53ca1ba5ee13775c7cba0e23362f67f5694675
Tomás Gonçalves <1220917@isep.ipp.pt>
2024-04-29 23:01:21 +0100

    Unit tests - application

---
+++ jobs4u.core/src/test/java/core/domain/application/RankTest.java
@@ -0,0 +1,38 @@
+package core.domain.application;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RankTest {
+
+    @Test
+    void testRankEquality() {
+        Rank rank1 = Rank.valueOf(1);
+        Rank rank2 = Rank.valueOf(1);
+        Rank rank3 = Rank.valueOf(2);
+
+        assertEquals(rank1, rank2); // Should be equal
+        assertNotEquals(rank1, rank3); // Should not be equal
+    }
+
+    @Test
+    void testRankHashCode() {
+        Rank rank1 = Rank.valueOf(1);
+        Rank rank2 = Rank.valueOf(1);
+
+        assertEquals(rank1.hashCode(), rank2.hashCode());
+    }
+
+    @Test
+    void testRankToString() {
+        Rank rank = Rank.valueOf(42);
+        assertEquals("42", rank.toString());
+    }
+
+    @Test
+    void testInvalidRank() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Rank.valueOf(-1);
+        });
+    }
+}

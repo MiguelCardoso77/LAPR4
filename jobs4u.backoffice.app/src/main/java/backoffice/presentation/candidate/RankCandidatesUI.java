53e910d8485e8146f6e92e97224f6cd0fdf9340b
Miguel <1220772@isep.ipp.pt>
2024-05-13 13:57:18 +0100

    Selection of jobInterview done #25

--- jobs4u.backoffice.app/src/main/java/backoffice/presentation/candidate/RankCandidatesUI.java
+++ jobs4u.backoffice.app/src/main/java/backoffice/presentation/candidate/RankCandidatesUI.java
@@ -2,6 +2,8 @@ package backoffice.presentation.candidate;
 
 import console.presentation.utils.ConsoleColors;
 import core.application.controllers.RankCandidatesController;
+import core.domain.application.Application;
+import core.domain.interview.JobInterview;
 import core.domain.jobOpening.JobOpening;
 import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
@@ -13,14 +15,17 @@ public class RankCandidatesUI extends AbstractUI {
 
     @Override
     protected boolean doShow() {
-        selectJobOpening();
+        JobOpening selectedJob = selectJobOpening();
+        Application selectedApplication = selectApplication(selectedJob);
+        JobInterview selectedInterview = selectJobInterview(selectedApplication);
 
         return true;
     }
 
-    private void selectJobOpening() {
+    private JobOpening selectJobOpening() {
         List<JobOpening> jobOpenings = theController.getAllJobOpenings();
 
+        System.out.println("Job Openings:");
         for (int i = 0; i < jobOpenings.size(); i++) {
             System.out.println((i + 1) + " - " + jobOpenings.get(i).jobReference());
         }
@@ -29,6 +34,50 @@ public class RankCandidatesUI extends AbstractUI {
         if (selectedNumber < 1 || selectedNumber > jobOpenings.size()) {
             System.out.println(ConsoleColors.RED + "Invalid number. Please enter a number between 1 and " + jobOpenings.size() + ConsoleColors.RESET);
         }
+
+        return jobOpenings.get(selectedNumber - 1);
+    }
+
+    private Application selectApplication(JobOpening selectedJob) {
+        List<Application> applications = theController.getApplicationsForJobOpening(selectedJob);
+
+        if (applications.isEmpty()) {
+            System.out.println(ConsoleColors.RED + "No applications found for the selected job opening." + ConsoleColors.RESET);
+            return null;
+        }
+
+        System.out.println("\nApplications:");
+        for (int i = 0; i < applications.size(); i++) {
+            System.out.println((i + 1) + " - " + applications.get(i).identity());
+        }
+
+        int selectedApplicationIndex = Console.readInteger("Please select an application by entering its number: ");
+        if (selectedApplicationIndex < 1 || selectedApplicationIndex > applications.size()) {
+            System.out.println(ConsoleColors.RED + "Invalid number. Please enter a number between 1 and " + applications.size() + ConsoleColors.RESET);
+        }
+
+        return applications.get(selectedApplicationIndex - 1);
+    }
+
+    private JobInterview selectJobInterview(Application selectedApplication) {
+        List<JobInterview> interviews = theController.getInterviewsForApplication(selectedApplication);
+
+        if (interviews.isEmpty()) {
+            System.out.println(ConsoleColors.RED + "No interviews found for the selected application." + ConsoleColors.RESET);
+            return null;
+        }
+
+        System.out.println("\nJob Interviews:");
+        for (int i = 0; i < interviews.size(); i++) {
+            System.out.println((i + 1) + " - " + interviews.get(i).identity());
+        }
+
+        int selectedInterviewIndex = Console.readInteger("Please select a job interview by entering its number: ");
+        if (selectedInterviewIndex < 1 || selectedInterviewIndex > interviews.size()) {
+            System.out.println(ConsoleColors.RED + "Invalid number. Please enter a number between 1 and " + interviews.size() + ConsoleColors.RESET);
+        }
+
+        return interviews.get(selectedInterviewIndex - 1);
     }
 
     @Override

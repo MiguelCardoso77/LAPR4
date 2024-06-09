b68693fc37098baa525822333633a4c20992c5a8
Diogo_1220812 <1220812@isep.ipp.pt>
2024-06-09 16:12:10 +0100

    Warning fixes #33

--- jobs4u.core/src/main/java/core/application/controllers/AddUserController.java
+++ jobs4u.core/src/main/java/core/application/controllers/AddUserController.java
@@ -6,6 +6,7 @@ import eapli.framework.application.UseCaseController;
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
 import eapli.framework.infrastructure.authz.application.UserManagementService;
+import eapli.framework.infrastructure.authz.application.UserSession;
 import eapli.framework.infrastructure.authz.domain.model.Role;
 import eapli.framework.infrastructure.authz.domain.model.SystemUser;
 import eapli.framework.time.util.CurrentTimeCalendars;
@@ -15,63 +16,82 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ * Controller for adding users.
+ * This class provides methods for adding users, generating passwords, getting all roles, and getting all users.
  *
- * Created by nuno on 21/03/16.
+ * @author 1220812@isep.ipp.pt
  */
 @UseCaseController
 public class AddUserController {
-
+    /**
+     * Authorization service instance.
+     */
     private final AuthorizationService authz = AuthzRegistry.authorizationService();
+    /**
+     * User management service instance.
+     */
     private final UserManagementService userSvc = AuthzRegistry.userService();
 
     /**
      * Get existing RoleTypes available to the user.
      *
-     * @return a list of RoleTypes
+     * @return an array of RoleTypes
      */
-
     public Role[] getAllRoles() {
         return Jobs4URoles.allValues();
     }
-
+    /**
+     * Get existing RoleTypes available to the user.
+     *
+     * @return a list of RoleTypes
+     */
     public String passwordGenerator(String name) {
         Jobs4UPasswordPolicy generator = new Jobs4UPasswordPolicy();
         return generator.passwordGenerator(name);
     }
-
+    /**
+     * Retrieves the currently logged-in user.
+     *
+     * @return The currently logged-in user, or null if no user is logged in.
+     */
     public SystemUser getLoggedInUser() {
-        return authz.session().map(session -> session.authenticatedUser()).orElse(null);
+        return authz.session().map(UserSession::authenticatedUser).orElse(null);
     }
-
-    public SystemUser addUser(String username, final String password, final String firstName, final String lastName, final String email, final Set<Role> roles, final Calendar createdOn) {
-        username = email;
+    /**
+     * Adds a new user with the provided details.
+     *
+     * @param password The password of the new user.
+     * @param firstName The first name of the new user.
+     * @param lastName The last name of the new user.
+     * @param email The email of the new user.
+     * @param roles The roles of the new user.
+     * @param createdOn The creation date of the new user.
+     * @return The created user.
+     */
+    public SystemUser addUser(final String password, final String firstName, final String lastName, final String email, final Set<Role> roles, final Calendar createdOn) {
         authz.ensureAuthenticatedUserHasAnyOf(Jobs4URoles.BOOTSTRAP, Jobs4URoles.ADMIN, Jobs4URoles.CUSTOMER_MANAGER, Jobs4URoles.OPERATOR);
 
-        return userSvc.registerNewUser(username, password, firstName, lastName, email, roles, createdOn);
+        return userSvc.registerNewUser(email, password, firstName, lastName, email, roles, createdOn);
     }
-
-    public SystemUser addUser(String username, final String password, final String firstName, final String lastName, final String email, final Set<Role> roles) {
-        username = email;
-        return addUser(username, password, firstName, lastName, email, roles, CurrentTimeCalendars.now());
+    /**
+     * Adds a new user with the provided details.
+     *
+     * @param password The password of the new user.
+     * @param firstName The first name of the new user.
+     * @param lastName The last name of the new user.
+     * @param email The email of the new user.
+     * @param roles The roles of the new user.
+     * @return The created user.
+     */
+    public SystemUser addUser(final String password, final String firstName, final String lastName, final String email, final Set<Role> roles) {
+        return addUser(password, firstName, lastName, email, roles, CurrentTimeCalendars.now());
     }
-
+    /**
+     * Retrieves all users.
+     *
+     * @return A list of all users.
+     */
     public List<SystemUser> allUsers() {
         return (List<SystemUser>) userSvc.allUsers();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

abe7d982d8f68ea0063b63e978e6cf6b08fba78f
Diana Neves <1221194@isep.ipp.pt>
2024-06-09 09:34:45 +0100

    javaDoc comments #26

--- jobs4u.backoffice.app/src/main/java/backoffice/presentation/application/UploadRequirementsAnswersAction.java
+++ jobs4u.backoffice.app/src/main/java/backoffice/presentation/application/UploadRequirementsAnswersAction.java
@@ -2,7 +2,19 @@ package backoffice.presentation.application;
 
 import eapli.framework.actions.Action;
 
+/**
+ * Action class responsible for executing the upload requirements answers action.
+ * It triggers the presentation of the UI for uploading candidate requirements answers.
+ *
+ * @author Diana Neves
+ */
 public class UploadRequirementsAnswersAction implements Action {
+
+    /**
+     * Executes the upload requirements answers action by displaying the UI for uploading candidate requirements answers.
+     *
+     * @return true if the requirement answers were successfully uploaded, false otherwise
+     */
     @Override
     public boolean execute() {
         return new UploadRequirementsAnswersUI().doShow();
